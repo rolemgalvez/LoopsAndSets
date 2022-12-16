04f5715be14262354405e2adb7ef9c6be2ed8cc6
Rolem Galvez <rolemgalvez@gmail.com>
2022-12-16 17:45:32 +0800

    Add project files.

---
+++ HomeworkFive/Program.cs
@@ -0,0 +1,39 @@
+﻿//Welcome message
+Console.WriteLine("Homework Five");
+Console.WriteLine("-------------");
+
+bool runAgain = true;
+
+while (runAgain)
+{
+    //Get user input
+    Console.WriteLine();
+    Console.Write("Enter your name (type \"exit\" to end program): ");
+    string? name = Console.ReadLine();
+
+    //Process input
+    if (name?.ToLower() == "exit")
+    {
+        runAgain = false;
+    }
+    else
+    {
+        runAgain = true;
+    }
+
+    string welcomeName = $"Student {name}";
+    if (name?.ToLower() == "tim")
+    {
+        welcomeName = $"Professor {name}";
+    }
+
+    //Show results
+    if (string.IsNullOrEmpty(name) == false)
+    {
+        Console.WriteLine($"Hi {welcomeName}!");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
