5d5d5db6744db0ebcfc63ef98d5a536fd8c8a4c2
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-20 14:44:42 +0800

    feature: on process creating HomeworkEight

---
+++ HomeworkEight/Program.cs
@@ -0,0 +1,16 @@
+﻿//Welcome message
+Console.WriteLine("Homework Eight");
+Console.WriteLine("--------------");
+
+//Declare variables
+Dictionary<int, string> records = new Dictionary<int, string>();
+bool runAgain;
+
+do
+{
+    runAgain = false;
+    Console.Write("\n[0] Add Record [1] Find Record [2] Exit: ");
+    string? choice = Console.ReadLine();
+
+
+} while (runAgain);
