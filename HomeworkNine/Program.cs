8d6b352b8ed198e6dffb448e3d77ce82fcb71461
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-24 11:32:36 +0800

    feature: add HomeworkNine project

---
+++ HomeworkNine/Program.cs
@@ -0,0 +1,25 @@
+﻿//Welcome message
+Console.WriteLine("Homework Nine");
+Console.WriteLine("-------------");
+Console.WriteLine();
+
+//Get input
+Console.Write("Enter first names (separated by comma): ");
+string userInput = Console.ReadLine();
+
+//Process input
+string[] firstNames = userInput.Split(',');
+
+//Show output
+if (firstNames.Length > 1)
+{
+	for (int i = 0; i < firstNames.Length; i++)
+	{
+		Console.WriteLine($"Hello {firstNames[i]}");
+	} 
+}
+
+//End
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
