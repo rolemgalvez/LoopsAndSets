24cf4969ffeeb71913eba3fdca1929ac3c69b585
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-25 09:25:48 +0800

    feature: add HomeworkTen project

---
+++ HomeworkTen/Program.cs
@@ -0,0 +1,29 @@
+﻿//Welcome
+Console.WriteLine("Homework Ten");
+Console.WriteLine("------------");
+Console.WriteLine();
+
+//Input
+List<string> firstNames = new();
+bool runAgain = true;
+
+while (runAgain)
+{
+    Console.Write("Enter first name: ");
+    string userInput = Console.ReadLine();
+    firstNames.Add(userInput);
+
+    runAgain = string.IsNullOrEmpty(userInput) == false;
+}
+
+//Output
+Console.WriteLine();
+foreach (string name in firstNames)
+{
+    Console.WriteLine($"Hello {name}!");
+}
+
+//End 
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
